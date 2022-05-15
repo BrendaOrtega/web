9866c1ae6f1e573f2f5a2a7a8a94bdf83b360c92
Brenda Gonzalez Ortega <brendago@MacBook-Pro-de-Brenda.local>
2022-05-14 20:54:38 -0500

    animations

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './HomePage.css';
-import { Link } from 'react-router-dom';
-import { Button } from "./Common/Button"
+import Fade from 'react-reveal/Fade';
 
 class About extends Component {
     componentDidMount() {
@@ -24,32 +23,35 @@ class About extends Component {
                 <div className="banner">
                 </div>
                 <section className="blog-box campus">
-                    <h2>Get to know T&P Campus</h2>
-                    <hr className="line" />
-                    <p>2020 came with different unexpected challenges, from a pandemic virus, to natural disasters.
-                        Technics Production B.V. took this as an opportunity to help those in need and created the T&P
-                        Campus. It is located in one of the most technologically driven cities in Europe, Eindhoven. It
-                        contributes to it, by hosting several small and medium size international organizations. Technics
-                        Production B.V. gives them all the necessary tools to establish and grow their projects.</p>
-                    <p> This technological hub has strived to become a sign of innovation across borders. Professionals
-                        from every industry and nationality come together in this campus to create, test and improve
-                        every single one of our products. Although it has not reached its goal yet, to become the most
-                        influential Tech Hub in the world, the T&P Campus is very well on its way to do it</p>
-                    <p>
-                        If you think your project or company should be in the T&P Campus,  <a href="mailto:info@technicsproduction.nl" aria-label="Mail" rel="noopener noreferrer"> <span>send us an email </span> </a> with a
-                        brief description of it and contact info. We will get in touch with you as soon as possible.
-                    </p>
-                    <div className="box-images">
-                        <div className="company-img img-1" alt="factory image"></div>
-                        <div className="company-img img-2" alt="factory image"></div>
-                        <div className="company-img img-3" alt="factory image"></div>
-                        <div className="company-img img-4" alt="factory image"></div>
-                        <div className="company-img img-5" alt="factory image"></div>
-                        <div className="company-img img-6" alt="factory image"></div>
-                        <div className="company-img img-7" alt="factory image"></div>
-                    </div>
+
+                    <Fade bottom>
+                        <h2>Get to know T&P Campus</h2>
+                        <hr className="line" />
+                        <p>2020 came with different unexpected challenges, from a pandemic virus, to natural disasters.
+                            Technics Production B.V. took this as an opportunity to help those in need and created the T&P
+                            Campus. It is located in one of the most technologically driven cities in Europe, Eindhoven. It
+                            contributes to it, by hosting several small and medium size international organizations. Technics
+                            Production B.V. gives them all the necessary tools to establish and grow their projects.</p>
+                        <p> This technological hub has strived to become a sign of innovation across borders. Professionals
+                            from every industry and nationality come together in this campus to create, test and improve
+                            every single one of our products. Although it has not reached its goal yet, to become the most
+                            influential Tech Hub in the world, the T&P Campus is very well on its way to do it</p>
+                        <p>
+                            If you think your project or company should be in the T&P Campus,  <a href="mailto:info@technicsproduction.nl" aria-label="Mail" rel="noopener noreferrer"> <span>send us an email </span> </a> with a
+                            brief description of it and contact info. We will get in touch with you as soon as possible.
+                        </p>
+                        <div className="box-images">
+                            <div className="company-img img-1" alt="factory image"></div>
+                            <div className="company-img img-2" alt="factory image"></div>
+                            <div className="company-img img-3" alt="factory image"></div>
+                            <div className="company-img img-4" alt="factory image"></div>
+                            <div className="company-img img-5" alt="factory image"></div>
+                            <div className="company-img img-6" alt="factory image"></div>
+                            <div className="company-img img-7" alt="factory image"></div>
+                        </div>
+                    </Fade>
                 </section>
-            </section>
+            </section >
         );
     }
 }
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,13 +3,12 @@ import './HomePage.css';
 import { Link } from 'react-router-dom';
 import { Carousel } from 'antd';
 import { Card } from "./Common/Card"
-import { Post } from "./Common/Post";
-import Footer from "./Footer";
 import { Button } from "./Common/Button"
 import logo from "../assets/logo.png"
 import image from "../assets/20201105_105139.jpg"
 import image2 from "../assets/20201208_161944.jpg"
 import image3 from "../assets/car.png"
+import Fade from 'react-reveal/Fade';
 
 function onChange(a, b, c) {
     console.log(a, b, c);
@@ -74,35 +73,37 @@ class HomePage extends Component {
                     </Carousel>
                 </div>
                 <section className="products-box">
-                    <h2>Our services</h2>
-                    <hr className="line" />
-                    <p className="text"> <strong>Technics Production BV </strong> develops everything in the field of applications and use of high-tech electronics. Thanks to the accumulated know-how, we are already playing a decisive role in the early stages of a development project.
-                        We have this function not only with regard to new products, but also for the improvement and adaptation of existing products.</p>
-                    <br />
-                    <br />
-                    <div className="flex-products">
-                        <Card
-                            image={image}
-                            name="Mentorship"
-                            description="What and how on hardware and software to bring your idea to fruition "
-                        />
-                        <Card
-                            image={image3}
-                            name="Production"
-                            description="If you already have an existing and finished product, we can assemble it,
+                    <Fade bottom>
+                        <h2>Our services</h2>
+                        <hr className="line" />
+                        <p className="text"> <strong>Technics Production BV </strong> develops everything in the field of applications and use of high-tech electronics. Thanks to the accumulated know-how, we are already playing a decisive role in the early stages of a development project.
+                            We have this function not only with regard to new products, but also for the improvement and adaptation of existing products.</p>
+                        <br />
+                        <br />
+                        <div className="flex-products">
+                            <Card
+                                image={image}
+                                name="Mentorship"
+                                description="What and how on hardware and software to bring your idea to fruition "
+                            />
+                            <Card
+                                image={image3}
+                                name="Production"
+                                description="If you already have an existing and finished product, we can assemble it,
                                 manufacture it in pieces and/or mass-produce it for you"
-                        />
-                        <section className="card">
-                            <div className="img_box">
-                                <img src={image2} alt="image-product" />
-                            </div>
-                            <div className="space">
-                                <h3>T&P Campus</h3>
-                                <p>Need a company or a place to begin your own creations, <span>click here</span> </p>
-                                <br />
-                            </div>
-                        </section>
-                    </div>
+                            />
+                            <section className="card">
+                                <div className="img_box">
+                                    <img src={image2} alt="image-product" />
+                                </div>
+                                <div className="space">
+                                    <h3>T&P Campus</h3>
+                                    <p>Need a company or a place to begin your own creations, <span>click here</span> </p>
+                                    <br />
+                                </div>
+                            </section>
+                        </div>
+                    </Fade>
                 </section>
                 <section className="clients-box">
                     <div className="clients">
@@ -117,31 +118,33 @@ class HomePage extends Component {
                     </div>
                 </section>
                 <section>
-                    <div className="container">
-                        <div className="company-info">
-                            <h2>About Technics Production BV</h2>
-                            <p>Technics Production B.V. began as a consultancy and outsourcing company for ground, water
-                                and road construction. It always leaned towards electronic solutions for these fields, to the point
-                                that it ventured in unimaginable areas (from traffic control, to equipment tracking, among several
-                                others). Just as unimaginable was the success it had, when providing unconventional but highly
-                                efficient solutions.
-                            </p>
-                            <p>Nowadays, Technics Production B.V. helps you grow the potential of your ideas, by developing
-                                a whole ecosystem (software and hardware) around them. It does not matter which industry you
-                                need it for, we go from mobile applications to high-end mechanics/electronics. We specialized in
-                                transforming ideas into realities. The wildest the dream, the better the challenge. Let us make
-                                your wishes come true. Tell us what you wish, and we will take care of how to do it.
-                                Do you want to get started, <a href="https://calendly.com/spikestinger/15min?month=2021-01" target="_blank" rel="noopener noreferrer"> <span>click here</span> </a> and we will get in touch with you as soon as possible.
-                            </p>
-                            <br />
-                            <br />
-                            <Link to="/contact">
-                                <Button text="Contact us" />
-                            </Link>
+                    <Fade bottom>
+                        <div className="container">
+                            <div className="company-info">
+                                <h2>About Technics Production BV</h2>
+                                <p>Technics Production B.V. began as a consultancy and outsourcing company for ground, water
+                                    and road construction. It always leaned towards electronic solutions for these fields, to the point
+                                    that it ventured in unimaginable areas (from traffic control, to equipment tracking, among several
+                                    others). Just as unimaginable was the success it had, when providing unconventional but highly
+                                    efficient solutions.
+                                </p>
+                                <p>Nowadays, Technics Production B.V. helps you grow the potential of your ideas, by developing
+                                    a whole ecosystem (software and hardware) around them. It does not matter which industry you
+                                    need it for, we go from mobile applications to high-end mechanics/electronics. We specialized in
+                                    transforming ideas into realities. The wildest the dream, the better the challenge. Let us make
+                                    your wishes come true. Tell us what you wish, and we will take care of how to do it.
+                                    Do you want to get started, <a href="https://calendly.com/spikestinger/15min?month=2021-01" target="_blank" rel="noopener noreferrer"> <span>click here</span> </a> and we will get in touch with you as soon as possible.
+                                </p>
+                                <br />
+                                <br />
+                                <Link to="/contact">
+                                    <Button text="Contact us" />
+                                </Link>
+                            </div>
+                            <div className="company-image" >
+                            </div>
                         </div>
-                        <div className="company-image" >
-                        </div>
-                    </div>
+                    </Fade>
                 </section>
 
             </section>
